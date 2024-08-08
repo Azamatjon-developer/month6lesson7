8a6f87747f834e4e59841ef5df12014f4d683777
master <https://github.com/Azamatjon-developer/exam5-medium.git>
2024-08-08 10:47:57 +0500

    PagesUpdate

--- src/routes/Dashboard/index.jsx
+++ src/routes/Dashboard/index.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import {  Home, Profile } from '../../pages/Dashboard'
 import Navbar from '../../components/Navbar'
+import Explore from '../../pages/Dashboard/Explore'
+import Notifications from '../../pages/Dashboard/Notifications'
+import Messages from '../../pages/Dashboard/Messages'
+import Bookmarks from '../../pages/Dashboard/Bookmarks'
+import Lists from '../../pages/Dashboard/Lists'
+import More from '../../pages/Dashboard/More'
 
 function DashboardPage() {
   return (
@@ -10,6 +16,12 @@ function DashboardPage() {
       <Routes>
         <Route path='/' element = {<Home/>} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/notifications" element={<Notifications />} />
+        <Route path="/messages" element={<Messages />} />
+        <Route path="/bookmarks" element={<Bookmarks />} />
+        <Route path="/lists" element={<Lists />} />
+        <Route path="/more" element={<More />} />
       </Routes>
     </div>
   )
